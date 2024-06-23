186a486f2249dfbaca222e134e86885aa5ce3b4e
Mehmet Emin ORHAN <mehmeteminorhan99@gmail.com>
2024-06-24 00:02:18 +0300

    MainPage created

--- src/components/Homepage/MainPage.jsx
+++ src/components/Homepage/MainPage.jsx
@@ -4,6 +4,40 @@ import './MainPage.css'
 
 export default function MainPage(){
 
+    const acikClick = () => {
+        history.push('/OrderPage');
+        
+    }
+
+    return (<>
+        <div className="main-container">
+        <img src="./Assets/mile1-assets/logo.svg" className="logo" />
+        <div className="tanitim">
+        <p
+            style={{
+              fontFamily: "Satisfy",
+              color: "#FDC913",
+              fontSize: "32px",
+              height: "40px",
+              width: "50%",
+              lineHeight: "44.27px",
+            }}
+          >
+            fırsatı kaçırma
+          </p>
+          <p>KOD ACIKTIRIR</p>
+          <p>PIZZA, DOYURUR</p>
+        </div>
+            <button onClick={acikClick} className='aciktim'> 
+                ACIKTIM
+            </button>
+            <img
+            src='./Assets/mile1-assets/home-banner.png'
+            className='main-image'
+        ></img>
+        </div>
+  
+    </>)
 }
 
 
