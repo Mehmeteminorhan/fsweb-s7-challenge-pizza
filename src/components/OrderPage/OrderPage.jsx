0f518a22499da542b9c241b6716d4108385d25a4
Mehmet Emin ORHAN <mehmeteminorhan99@gmail.com>
2024-07-03 19:46:41 +0300

    orderPage was created

--- src/components/OrderPage/OrderPage.jsx
+++ src/components/OrderPage/OrderPage.jsx
@@ -1,8 +1,53 @@
-import { Link, NavLink } from "react-router-dom/cjs/react-router-dom.min";
-import React from 'react'
+import { Link, NavLink, useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import React, { useState, useTransition } from 'react'
 import './OrderPage.css'
 
+
+const icindekilerList = [
+    "Pepperoni", "Sosis", "Kanada Jambonu", "Tavuk Izgara",
+    "Soğan", "Domates", "Mısır", "Sucuk", "Jalepeno", "Sarımsak",
+    "Biber", "Mozarella", "Ananas", "Kabak"
+];
+
+
+
 export default function OrderPage(){
+    const [pizza,setPizza] = useState('');
+    const [size,setSize] = useState('');
+    const [kalinlik, setKalinlik] = useState('');
+    const [extraicerik,setExtraicerik] = useState([]);
+    const [note,setNote] = useState('');
+    const [sayi,setSayi] = useState(1);
+    const [hata,setHata] = useState({});
+    const ucret = 85.5;
+    const extraucret = 5;
+    const history = useHistory();
+    
+    const IngredientChange = (ingredient) => {
+        setExtraicerik((prev) => {
+            if(prev.includes(ingredient)){
+                return prev.filter((item) => item !== ingredient);
+            }else{
+                if(prev.length < 10) {
+                    return [...prev,ingredient]
+                }else {
+                    return prev
+                }
+            }
+
+        })}
+
+
+
+
+
+
+
+
+
+
+
+
     return(
         <>
         <div className="order-header">
@@ -15,7 +60,7 @@ export default function OrderPage(){
             </header>
         </div>
         <main className="order-page">
-            <h2>hello</h2>
+            <h2>Position Absolute Acı Pizza</h2>
         </main>
         </>
     )
